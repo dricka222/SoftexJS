01527eacd70e6a50f1ed1803736d6bde345af489
adriely <drickas222@gmail.com>
2024-07-24 11:46:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+/* let users = ["Ruan", "Rinaldo", "Augusto"];
+
+// let maping = users.map(String);
+
+users.push("Isaell");
+
+console.log(users);
+
+users.pop();
+
+console.log(users);
+
+users.shift();
+
+console.log(users);
+
+users.unshift("Ruan");
+
+console.log(users); */
+
+/*let fruits = ["apple", "banana", "orange"];
+
+// Adicionando 'kiwi' ao final do array
+fruits.push("kiwi");
+
+console.log(fruits);
+
+// Removendo o último elemento
+fruits.pop();
+
+console.log(fruits);
+
+// Adicionando 'strawberry' no inicio do array
+fruits.unshift("strawberry");
+
+console.log(fruits);
+
+// Removendo o primeiro elemento do array
+fruits.shift();
+
+console.log(fruits);
+
+fruits.push("melancia");
+
+console.log(fruits);
+console.log(
+  "Em que posição a melancia está no array ? : R = " +
+    fruits.indexOf("melancia")
+);
+
+console.log("Existe 'banana' no array ? : R = " + fruits.includes("banana"));*/
+
+// SEGUNDA ATIVIDADE
+
+// console.log("Método .map");
+
+// let numbers = [1, 2, 3, 4];
+
+// let squaredNumbers = numbers.map((num) => num * num);
+
+// console.log(squaredNumbers);
+
+// console.log("");
+
+// console.log("Método .filter");
+
+// console.log("");
+
+// let newNumbers = [1, 2, 3, 4];
+
+// let eventNumber = numbers.filter((num) => num % 2 === 0);
+
+// console.log(eventNumber);
+
+
+// TERCEIRA ATIVIDADE
+
+// const produtos = [
+//   { id: 1, nome: "camisa" },
+//   { id: 2, nome: "calça" },
+//   { id: 3, nome: "sandálias" },
+// ];
+
+// function encontrarProdutosPorId(id) {
+//   const produtoEncontrado = produtos.find((produto) => produto.id == id);
+
+//   if (!produtoEncontrado) {
+//     return 'Produto não encontrado no bacno de dados';
+
+//   }
+
+//   return produtoEncontrado;
+
+// }
+
+// console.log(encontrarProdutosPorId(10));
+
+
